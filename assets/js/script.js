a51b4812d31301d84706b0fc5df407bfeca7adb8
hszilvi <szilvi@naszta.hu>
2024-01-15 12:29:26 +0000

    all btns work and set local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
+const myKey = '';
 
+  let displayedCity ='London';
 
-    const myKey = '';
-    // const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=London,England&appid=${myKey}`
-    const cities = []
     // first get the geo data, like city from fetch and console.log all the needed data - just to check if it works!
     const cityCurrent = $('#displayed-city');
     const dayZero = $('#displayed-date');
@@ -15,133 +14,92 @@
     // function to update default data in today section--------------------- OK OK OK OK 
     function defaultToday(city) {
         const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${myKey}`
-
         // ----------------------------------------------
         fetch(queryURL).then(function (response) {
+
             return response.json();
-            }).then(function (data) {
+            }).then(function (data) {  
+                console.log(data[0].lat, data[0].lon); //check coordinates
+                // getting data for 0 and + 5 days
                 // need another fetch to get the weather data
                 console.log(data);  
                 const newQueryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=${myKey}&units=metric`;
+
                     fetch(newQueryURL).then(function (response) {
                         return response.json();
                     }).then(function (data) {
+
                         cityCurrent.text(data.city.name);
                         dayZero.text(dayjs(data.list[0].dt_txt).format('D/M/YYYY'));
-                        // dayZero.text(data.list[0].dt_txt);
                         temp.text(Math.round(data.list[0].main.temp));
                         hum.text(data.list[0].main.humidity);
                         wind.text(Math.round(data.list[0].wind.gust));
                         descr.text(data.list[0].weather[0].description);
                         icon = data.list[0].weather[0].icon;
-                        $('#displayed-icon').attr("src",  `https://openweathermap.org/img/wn/${icon}@4x.png`);
-                        // to change icon I need a different fetch to get the pics
-                        // icon.src = 
+
+                        $('#displayed-icon').attr("src",  `https://openweathermap.org/img/wn/${icon}@4x.png`); // display img using icon variable that stores icon id, 4x at the end of http contains the size
                         console.log(data);
                         // console.log(data.city.name);
                         console.log(data.list[0].dt_txt); //current data and time
                         console.log(data.list[0].weather[0].icon); // icon id
-                        // console.log(data.list[0].main.temp)
-                        // console.log(data.list[0].main.humidity)
-                        // console.log(data.list[0].wind.speed)
+                        // get five day forecast and pass data to html
+
             })
         })
     }
-    defaultToday('hanoi');
-
-    // let inputCity = document.getElementById('displayed-city').innerHTML.trim();
-    // function fetchCity () {
-    //     const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=London,England&appid=${myKey}`;
-    //     fetch(queryURL).then(function (response) {
-    //         return response.json();
-    //     }).then(function (data) {
-    //         console.log(data);  
-    //     })
-
-    // }
-
-    // function addCityBtn () {
-
-    // }
-    // add eventlistener to search button to get city data
-    // const searchBtn = document.getElementById('search-button');
-    // searchBtn.addEventListener('click', () => {
-    //     fetchCity();
-    // })
+    
 
     // create button elements from user City inputs OK OK OK
     function renderBtns() {
         $('#history').empty();
-        $.each(cities, function(i, city) {
+        $.each(searchedCities, function(i, city) {
             const createBtnDiv = $('<div>').addClass('city-input input-group justify-content-center mb-1 mt-2');
             const createCityBtn = $('<button>').addClass("city-button btn btn-outline-danger").attr('data-name', city).text(city);
             // createBtnDiv.append(createCityBtn);
             $('#history').prepend(createBtnDiv.append(createCityBtn));
         })
     }
-    // const storedCities = JSON.parse(localStorage.getItem('cities')) || []
-    // search button is clicked call renderBtns function and push cities into cities array OK OK OK 
+    const storedCities = JSON.parse(localStorage.getItem('searched-cities')) || []
+    // search button is clicked call renderBtns function and push cities into cities array and update displayedCity 
+    let searchedCities = storedCities;
     $('#search-button').on('click', function(e) {
         e.preventDefault();
         const userCity = $('#search-input').val().trim();
         console.log(userCity);
-        // check to avoid duplication and don't have empty buttons
-        if (!cities.includes(userCity) && userCity) {
-            cities.push(userCity);
+        if (!searchedCities.includes(userCity) && userCity)  { // check to avoid duplication and don't have empty buttons
+            searchedCities.push(userCity);
         }
-        console.log(cities);
-        // empty input val to replace placeholder
-        $('#search-input').val('')
-
-        // store data in local storage
-        localStorage.setItem('cities', cities);
-        // cities = JSON.parse(localStorage.getItem(userCity));
-        // call render buttons
+        console.log(searchedCities);
+        $('#search-input').val('') // empty input val to replace placeholder
+        localStorage.setItem('searched-cities', JSON.stringify(searchedCities));// store data in local storage
+        // call render buttons to display cities as buttons in html
         renderBtns();
     })
-    // renderBtns();
+    renderBtns();
 
-    // clear cities list / need to rewrite in jquery!!!!! else it works OK OK OK 
-    const clearBtn = document.getElementById('clear-button');
-    clearBtn.addEventListener('click', (e) => {
+    // clear cities list and local storage
+   $('#clear-button').on('click', function(e) {
         e.preventDefault();
         console.log('clear is clicked');
         $('#history').empty();
+        localStorage.clear();
+        searchedCities = [];
     })
 
-    $(document).on('click', '.city-button', consoleTest);
+    $(document).on('click', '.city-button', setChosenCityToDisplay);
     
+    function setChosenCityToDisplay() {
+        const chosenCity = $(this).attr('data-name');
+        console.log(chosenCity);
+        displayedCity = chosenCity;
+        renderBtns();
+        return displayedCity;
 
-    function consoleTest() {
-        console.log('just fluffing')
     }
+    console.log(displayedCity);
 
 
-    // fetch(queryURL).then(function (response) {
-    //     return response.json();
-    //     }).then(function (data) {
-    //         // need another fetch to get the weather data
-    //         console.log(data);  
-    //         const newQueryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=${myKey}&units=metric`;
-    //             fetch(newQueryURL).then(function (response) {
-    //                 return response.json();
-    //             }).then(function (data) {
-    //                 console.log(data);
-    //                 console.log(data.city.name);
-    //                 console.log(data.list[0].dt_txt); //current data and time
-    //                 console.log(data.list[0].weather[0].icon); // icon id
-    //                 console.log(data.list[0].main.temp)
-    //                 console.log(data.list[0].main.humidity)
-    //                 console.log(data.list[0].wind.speed)
-    //     })
-    //     const cities = ["London", "Budapest"];
-    // })
-
-    // update data with default city London on website 
-
-
-
-
+    defaultToday(displayedCity);
 
     // function that creates buttons with city names from input field (id search-input) form and post it to 
         // search button id search-button clicked store the input field local storage and create form element with id history             
